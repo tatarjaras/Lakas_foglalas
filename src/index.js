ad7329da3c0c881f1b347f272d867dada995d15c
tatarjaras <havasim@kkszki.hu>
2025-02-13 12:24:42 +0100

    index.js-be beraktunk egy kikommentelt próbát

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {App} from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+ //próba
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
